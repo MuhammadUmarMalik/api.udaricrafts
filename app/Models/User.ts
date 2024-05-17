4b23a63087bcf1ea76828960e906687efa02e7a1
Muhammad Umar Malik <89419975+MuhammadUmarMalik@users.noreply.github.com>
2024-05-17 06:43:37 +0500

    create a adonis project and work on admin , where work on user table, authentication and seeders

---
+++ app/Models/User.ts
@@ -0,0 +1,20 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class User extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+  @column()
+  name: string
+  @column()
+  email: string
+  @column()
+  password: string
+  @column()
+  role: string
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
